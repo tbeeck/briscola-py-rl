24659e46a553d3824d7ae13acbe3491a28bbd184
Tim Beck <socaltdb@gmail.com>
2025-03-10 16:56:26 -0700

    skeleton for environment

---
+++ briscola_env/briscola_env.py
@@ -0,0 +1,35 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+
+
+class BriscolaEnv(gym.Env):
+    """Custom Environment that follows gym interface."""
+
+    metadata = {"render_modes": ["human"], "render_fps": 30}
+
+    def __init__(self):
+        super().__init__()
+        # 3 cards to play (need to mask in the case that the player has less than 3 cards)
+        self.action_space = spaces.Discrete(3)
+        # Our hand (3)
+        # The trick so far (3)
+        # The Briscola (1)
+        # Cards already played (40)
+        # our points (1)
+        # opponent points (3)
+        self.observation_space = spaces.Box(
+            low=0, high=255, shape=(3 + 3 + 1 + 40 + 1 + 3,), dtype=np.uint8
+        )
+
+    def step(self, action):
+        return observation, reward, terminated, truncated, info
+
+    def reset(self, seed=None, options=None):
+        return observation, info
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
