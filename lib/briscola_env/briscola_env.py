ec33ac5b6666be307a3de71bda838a5df46a2c95
Tim Beck <socaltdb@gmail.com>
2025-04-26 13:09:51 -0700

    action masking next

--- lib/briscola_env/briscola_env.py
+++ lib/briscola_env/briscola_env.py
@@ -1,9 +1,14 @@
+import functools
 import numpy as np
 from gymnasium import spaces
 
 from lib.briscola.game import BriscolaGame
 from lib.briscola_env.embedding import card_reverse_embedding, game_embedding
 from pettingzoo import AECEnv
+
+
+def player_id(player: str) -> int:
+    return int(player.replace("player_", ""))
 
 
 class BriscolaEnv(AECEnv):
@@ -13,24 +18,12 @@ class BriscolaEnv(AECEnv):
 
     def __init__(self):
         super().__init__()
-        # 40 possible cards to play
-        # need to mask the space to the available cards in hand
-        self.action_space = spaces.Discrete(40)
-        # Tips on observation space embedding:
-        # https://rlcard.org/games.html
-        # Our hand (3)
-        # The trick so far (3)
-        # The Briscola (1)
-        # Cards already played (40)
-        # our points (1)
-        # opponent points (3)
-        self.observation_space = spaces.Box(
-            low=0, high=255, shape=(3 + 3 + 1 + 40 + 1 + 3,), dtype=np.uint8
-        )
-        self.agents = [i for i in range(4)]
+        self.possible_agents = [f"player_{i}" for i in range(4)]
 
     def step(self, action):
+        agent = self.agent_selection
         played_card = card_reverse_embedding(action)
+
         self.game.play(played_card)
         if self.game.should_score_trick():
             self.game.score_trick()
@@ -43,18 +36,44 @@ class BriscolaEnv(AECEnv):
             terminated = True
             if self.game.leaders()[0] == 0:
                 reward = 1
-        observation, info = self.observe()
+        observation, info = self.observe(agent)
 
         return observation, reward, terminated, {}, info
 
     def reset(self, seed=None, options=None):
         self.game = BriscolaGame(players=4, goes_first=0, seed=seed)
-        observation, info = self.observe()
-        return observation, info
+        self.agents = self.possible_agents[:]
+        self.rewards = {agent: 0 for agent in self.agents}
+        self.terminations = {agent: False for agent in self.agents}
+        self.truncations = {agent: False for agent in self.agents}
+        self.infos = {agent: {} for agent in self.agents}
+        self.observations = {agent: self.observe(agent) for agent in self.agents}
+        self.agent_selection = self.agents[0]
+        self._cumulative_rewards = {agent: 0 for agent in self.agents}
 
     def render(self):
         print(self.game)
 
-    def observe(self):
-        observation = {"observation": game_embedding(self.game, 0)}
+    def observe(self, agent):
+        observation = {"observation": game_embedding(self.game, player_id(agent))}
         return observation, {}
+
+    @functools.lru_cache(maxsize=None)
+    def observation_space(self, agent):
+        # Tips on observation space embedding:
+        # https://rlcard.org/games.html
+        # Our hand (3)
+        # The trick so far (3)
+        # The Briscola (1)
+        # Cards already played (40)
+        # our points (1)
+        # opponent points (3)
+        return spaces.Box(
+            low=0, high=255, shape=(3 + 3 + 1 + 40 + 1 + 3,), dtype=np.uint8
+        )
+
+    @functools.lru_cache(maxsize=None)
+    def action_space(self, agent):
+        # 40 possible cards to play
+        # need to mask the space to the available cards in hand
+        return spaces.Discrete(40)
